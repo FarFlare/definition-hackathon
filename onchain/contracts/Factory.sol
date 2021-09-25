b5b01e110934fcc7ccbdea11654e7f9626e29ebb
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2021-09-25 16:24:20 +0300

    DAO & Factory completed, but not tested

--- onchain/contracts/Factory.sol
+++ onchain/contracts/Factory.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "./Pool.sol";
 import "./Share.sol";
 import "./Dao.sol";
@@ -7,23 +8,21 @@ import "./Dao.sol";
 contract Factory {
 
     address public initiator;  // Offchain initiator
-    Pool public pool;
     address public owner;  // Will allow to setup offchain initiator
+    Pool public pool;
 
     modifier ownerOnly {require(msg.sender == owner); _;}
     modifier initiatorOnly {require(msg.sender == owner); _;}
 
-    constructor() {
+    constructor(Pool _pool) {
         owner = msg.sender;
         initiator = msg.sender;
+        pool = _pool;
     }
 
-    function setup(address _new_initiator, Pool _new_pool) public ownerOnly {
+    function setup(address _new_initiator) public ownerOnly {
         if (_new_initiator != address(0x0)) {
             initiator = _new_initiator;
-        }
-        if (address(_new_pool) != address(0x0)) {
-            pool = _new_pool;
         }
     }
 
@@ -31,11 +30,9 @@ contract Factory {
         //  Setting up the emission
         IERC20 dao_token = new Share("Crowd Shares", "CS", _shares_amount);
         //  Building the brand new DAO
-        Dao dao = new Dao("name", dao_token);
-
-        //  Sending tokens to Pool for distribution
-        dao_token.transfer(address(pool), dao_token.totalSupply());
-        pool.distribute_shares(nft_address, nft_id, dao_token);
+        Dao dao = new Dao("name", dao_token, pool);
+        //  Sending tokens to Dao for distribution
+        dao_token.transfer(address(dao), dao_token.totalSupply());
     }
 
 }
