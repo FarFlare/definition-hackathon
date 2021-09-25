f75186dd7a0e4b3caf045400d87398453ca002e7
Tamara Zhordanija <rysshe@Tamaras-MacBook-Air.local>
2021-09-25 14:52:29 +0300

    Pool v0.1

--- onchain/contracts/Pool.sol
+++ onchain/contracts/Pool.sol
@@ -9,11 +9,12 @@ contract Pool {
     // For each NFT address and it's ID store the ETH amount for each user address. 
     // {NFT_address => {NFT_id => {sender_ID => ETH_amount}}}
     mapping(IERC721 => mapping(uint => mapping(address => uint))) public pool;
-    event NewDeposit(IERC721 indexed nft_address, uint nft_id, address owner, uint deposit);
-    event NewClaim(IERC721 indexed nft_address, uint nft_id,  uint deposit);
+    mapping(IERC721 => mapping(uint => uint)) public pool_total;
+    event NewDeposit(IERC721 indexed nft_address, uint nft_id, address sender, uint deposit);
+    event DistributionShare(IERC721 indexed nft_address, uint nft_id, address sender);
+    event TotalShare(IERC721 indexed nft_address, uint nft_id, address sender);
 
-    // ? clarify about class construction in solidity
-    // is the class coustructed once or every transaction? (1 probably)
+
     constructor(){
         owner = msg.sender;
     }
@@ -25,20 +26,33 @@ contract Pool {
         emit NewDeposit(nft_address, nft_id, msg.sender, msg.value);
     }
 
-    function get_amount(IERC721 nft_address, uint nft_id, address owner) internal view returns (uint) {
+    function get_amount(IERC721 nft_address, uint nft_id, address owner) public view returns (uint) {
         /*
         */
         return pool[nft_address][nft_id][owner];
     }
 
-    function claim(IERC721 nft_address, uint nft_id, IERC20 buyer_account) public view returns (bool){
+    function get_distribution(IERC721 nft_address, uint nft_id) public view returns(mapping(address => uint)){
         /*
         */
-        uint sender_amount = pool[nft_address][nft_id][msg.sender];
-        require(sender_amount == 0, "Claiming address is not a buyer.");
+        emit DistributionShare(nft_address, nft_id, msg.sender);
+        return pool[nft_address][nft_id];
+    }
 
-        // Count the proportion.
-        
+    function get_total(IERC721 nft_address, uint nft_id) public view returns(uint) {
+        /*
+        */
+        emit TotalShare(nft_address, nft_id, msg.sender);
+        return pool_total[nft_address][nft_id];
+    }
+
+    function get_stats(IERC721 nft_address, uint nft_id) public view returns (mapping(address => uint), uint) {
+        /*
+        */
+        mapping(address => uint) distribution = get_distribution(nft_address, nft_id);
+        uint total_sum = get_total(nft_address, nft_id);
+        return (distribution, total_sum);
+
     }
 
 }
