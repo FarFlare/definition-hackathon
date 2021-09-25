b3f3131c1f0cb5237c9ab27e8c0f639f1c2ea158
k0rean-rand0m <15913538+k0rean-rand0m@users.noreply.github.com>
2021-09-25 11:50:55 +0300

    Trying Open Zeppelin

--- onchain/contracts/Pool.sol
+++ onchain/contracts/Pool.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
 contract Pool {
     address public senderAddress;
 
-    function distribute_shares(IERC721 nft_address, uint nft_id, IERC20 shares);
+    function distribute_shares(IERC721 nft_address, uint nft_id, ERC20Votes shares) internal {}
 
 }
