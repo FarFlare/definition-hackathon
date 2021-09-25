673263ad7f5d5c17dfa0bad2e249a2fcbc80d863
Tamara Zhordanija <rysshe@Tamaras-MacBook-Air.local>
2021-09-25 16:40:13 +0300

    Pool v0.2

--- onchain/contracts/Pool.sol
+++ onchain/contracts/Pool.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import "./SafeMath.sol";
 
 
 contract Pool {
     address public owner;
-    // For each NFT address and it's ID store the ETH amount for each user address. 
-    // {NFT_address => {NFT_id => {sender_ID => ETH_amount}}}
+    // For each `IERC721` and it's ID store the WEI amount deposited by user. 
+    // {IERC721 => {IERC721_id => {sender => WEI}}}
     mapping(IERC721 => mapping(uint => mapping(address => uint))) public pool;
+    // Store the total deposit amount for each `IERC721` token.
     mapping(IERC721 => mapping(uint => uint)) public pool_total;
+    // Declare the events.
     event NewDeposit(IERC721 indexed nft_address, uint nft_id, address sender, uint deposit);
     event DistributionShare(IERC721 indexed nft_address, uint nft_id, address sender);
     event TotalShare(IERC721 indexed nft_address, uint nft_id, address sender);
@@ -21,38 +22,44 @@ contract Pool {
 
     function set_deposit(IERC721 nft_address, uint nft_id) payable {
         /*
+        Receive the payment and update the pool statistics.
         */
         pool[nft_address][nft_id][msg.sender] += msg.value;
+        pool_total[nft_address][nft_id] += msg.value;
         emit NewDeposit(nft_address, nft_id, msg.sender, msg.value);
     }
 
-    function get_amount(IERC721 nft_address, uint nft_id, address owner) public view returns (uint) {
+    function get_absolute(IERC721 nft_address, uint nft_id, address sender) public view returns (uint) {
         /*
+        Get the absolute amount of WEI deposited by the specified user to the target `IERC721` token.
         */
-        return pool[nft_address][nft_id][owner];
+        return pool[nft_address][nft_id][sender];
     }
 
-    function get_distribution(IERC721 nft_address, uint nft_id) public view returns(mapping(address => uint)){
+    function get_distribution(IERC721 nft_address, uint nft_id) public view returns (mapping(address => uint)) {
         /*
+        Get the user=>WEI mapping for the target `IERC721` token.
         */
+        dist = pool[nft_address][nft_id];
         emit DistributionShare(nft_address, nft_id, msg.sender);
-        return pool[nft_address][nft_id];
+        return dist; 
     }
 
     function get_total(IERC721 nft_address, uint nft_id) public view returns(uint) {
         /*
+        Get the total deposit sum for the target `IERC721` token.
         */
+        total = pool_total[nft_address][nft_id];
         emit TotalShare(nft_address, nft_id, msg.sender);
-        return pool_total[nft_address][nft_id];
+        return total;
     }
 
     function get_stats(IERC721 nft_address, uint nft_id) public view returns (mapping(address => uint), uint) {
         /*
+        Get distribution stats and total deposit for the `IERC721` by one call.
         */
         mapping(address => uint) distribution = get_distribution(nft_address, nft_id);
         uint total_sum = get_total(nft_address, nft_id);
         return (distribution, total_sum);
-
     }
-
 }
